8b75471aaf45a7a67e586708cfb294f5d687cdc1
MichealWind <504307283@qq.com>
2018-06-10 20:53:46 +0800

    之前git合并文件存在冲突，予以改正

--- applet/app.js
+++ applet/app.js
@@ -10,56 +10,42 @@ App({
   onLaunch: function () {
     this.globalData.login_code = wx.getStorageSync('login_code');
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    var logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
 
-<<<<<<< HEAD
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        requests.userLogin(res.code, (data) => {
-          if (data.length == 0) { 
-            console.log('登录失败')
-          } else {
-            console.log("登录成功，用户ID为：" + data.userId)
-            // 赋予userId正确的值
-            this.data.userId = data.userId
-          }
-        });
-=======
+    this.data.userId = wx.getStorageSync('userId') || '';
+
+    // 检查登录状态
     wx.checkSession({
       success: () => {
 
       },
       fail: () => {
+        wx.removeStorageSync('userId');
         wx.login({
           success: res => {
             if (res.code) {
-              // 发送 res.code 到后台换取 openId, sessionKey, unionId
-              /*
-              wx.request({
-                url: '', //后台的登录url，等后台做好了填上
-                data: {
-                  code: res.code
-                },
-                success: res => {
-                  //将openId存在数据缓存中用于后续操作
+              requests.userLogin(res.code, (data) => {
+                if (data.length == 0) {
+                  console.log('登录失败')
+                } else {
+                  console.log("登录成功，用户ID为：" + data.userId)
+                  // 赋予userId正确的值
+                  this.data.userId = data.userId
                   wx.setStorage({
-                    key: 'login_code',
-                    data: 
+                    key: 'userId',
+                    data: data.userId
                   });
                 }
-              })*/
+              });
             } else {
               // 登陆失败
             }
           }
         })
->>>>>>> 5c6310be265baa88a118c8486c626873277faffc
       }
-    })
+    });
 
     // 获取用户信息
     wx.getSetting({
@@ -85,10 +71,7 @@ App({
               wx.getUserInfo({
                 success: res => {
                   // 可以将 res 发送给后台解码出 unionId
-                  this.globalData.userInfo = res.userInfo
-
-                  
-
+                  this.globalData.userInfo = res.userInfo;
                   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
                   // 所以此处加入 callback 以防止这种情况
                   if (this.userInfoReadyCallback) {
@@ -96,9 +79,6 @@ App({
                   }
                 }
               })
-            },
-            fail: () => {
-              
             }
           })
         }
